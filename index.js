37e6b3a96140c1e6638a084cf675dffa3d1c2638
Alex Yatseyko <alex.yatseikogmail.com>
2020-02-25 18:09:29 +0200

    Prod

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const path = require('path')
 const bodyParser = require('body-parser')
 
 const app = express()
@@ -11,6 +12,13 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use('/api/comments', require('./routes/comments.route.js'))
 
+if (process.env.NODE_ENB === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = /* process.env.PORT || */ 5000
 const mongoUri = 'mongodb://admin:12345678a@ds033380.mlab.com:33380/test-app-ya'
